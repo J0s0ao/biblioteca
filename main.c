69e6c561cc7a3490de7277dd07b548fd695956fb
João Vitor Raimundi <joaoraimundi@gmail.com>
2026-05-15 19:50:06 -0300

    estrutura básica do C

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "biblioteca.h"
+
+int main() {
+    printf("Bem-vindo à biblioteca!\n");
+
+void adicionarLivro() {
+    // Lógica para adicionar um livro à biblioteca
+};
+
+void listarLivros() {
+    // Lógica para listar os livros disponíveis na biblioteca
+};
+
+void emprestarLivro() {
+    // Lógica para emprestar um livro da biblioteca 
+};
+
+void devolverLivro() {
+    // Lógica para devolver um livro à biblioteca
+};
+
+
+
+    return 0;
+};
