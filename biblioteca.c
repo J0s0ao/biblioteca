31ccebb0943576c6b1da11a22be0b5cdb796809f
João Vitor Raimundi <joaoraimundi@gmail.com>
2026-05-15 20:00:20 -0300

    modularização

--- biblioteca.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "biblioteca.h"
-
--- main.c
+++ main.c
@@ -4,24 +4,7 @@
 
 int main() {
     printf("Bem-vindo à biblioteca!\n");
-
-void adicionarLivro() {
-    // Lógica para adicionar um livro à biblioteca
-};
-
-void listarLivros() {
-    // Lógica para listar os livros disponíveis na biblioteca
-};
-
-void emprestarLivro() {
-    // Lógica para emprestar um livro da biblioteca 
-};
-
-void devolverLivro() {
-    // Lógica para devolver um livro à biblioteca
-};
-
-
+    
 
     return 0;
 };
