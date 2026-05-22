6f390d112f6dd839e9f8090c131c45179bf3b7f1
João Vitor Raimundi <joaoraimundi@gmail.com>
2026-05-22 19:23:56 -0300

    Alteração no usuário

--- usuario/usuario.h
+++ usuario/usuario.h
@@ -7,7 +7,9 @@ void liberar_lista_usuario(ListaUsuario* lista_usuario);
 void remover_usuario(ListaUsuario* lista_usuario, int codigo);
 void editar_usuario(ListaUsuario* lista_usuario, int codigo, char* novo_nome, char* nova_senha);
 Usuario* buscar_usuario(ListaUsuario* lista_usuario, int codigo);
-void listar_usuario(ListaUsuario* lista_usuario, int codigo);
+Usuario** buscar_usuario_por_nome(ListaUsuario* lista_usuario, char* pesquisa);
+ListaUsuario* adicionar_usuario(ListaUsuario* lista_usuario, int codigo, char* nome, char* senha);
+
 
 
 
